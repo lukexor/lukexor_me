c0da8a97e6df36b241b842ffa90b859c0b5f957f
Luke Petherbridge <lukexor@gmail.com>
2014-08-13 14:21:36 -0700

    Added fabfile

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import local
+from fabric.api import lcd
+
+# def prepare_deployment(branch_name):
+#     local('python manage.py test main')
+#     local('python manage.py test blog')
+#     local('git add -p && git commit')
+#     local('git checkout master && git merge ' + branch_name)
+
+def deploy():
+    with lcd('/path/to/my/prod/area/'):
+        # local('git pull /my/path/to/dev/area/')
+        local('python manage.py migrate main')
+        local('python manage.py migrate blog')
+        local('python manage.py test main')
+        local('python manage.py test blog')
+        local('/my/command/to/restart/webserver')
