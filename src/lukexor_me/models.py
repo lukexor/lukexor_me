317ba1dbd718831ecabc7e413fb370dda747bb19
Luke Petherbridge <lukexor@gmail.com>
2014-11-03 09:12:21 -0800

    Added models

--- src/lukexor_me/models.py
+++ src/lukexor_me/models.py
@@ -1,3 +1,179 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.core.mail import send_mail
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
+from lukexor_me import settings
 
-# Create your models here.
+
+class Article(models.Model):
+    article_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    permalink_title = models.CharField(max_length=45, unique=True)
+    body = models.TextField(blank=True)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='articles',
+                                related_query_name='article')
+    category = models.ForeignKey('Category', related_name='articles',
+                                    related_query_name='article', blank=True,
+                                    null=True, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField('Tag', related_name='articles',
+                                  related_query_name='article')
+    time_to_read = models.CharField(max_length=30, blank=True)
+    comments = models.ManyToManyField('Comment', related_name='articles',
+                                       related_query_name='article')
+    comment_count = models.PositiveIntegerField('# of comments', default=0)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'article'
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
+
+    def __unicode__(self):
+        return self.title
+
+
+
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'category'
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __unicode__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    body = models.TextField(blank=True)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'comment'
+        verbose_name = 'comment'
+        verbose_name_plural = 'comments'
+
+    def __unicode__(self):
+        return "%s - %s" % (self.body, self.user)
+
+
+class CustomUserManager(BaseUserManager):
+
+    def _create_user(self, email, password,
+                     is_staff, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+        if not email:
+            raise ValueError('The given email must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=is_staff, is_active=True,
+                          is_superuser=is_superuser, last_login=now,
+                          date_joined=now, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False,
+                                 **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True,
+                                 **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+
+    """
+    A fully featured User model with admin-compliant permissions that uses
+    a full-length email field as the username.
+
+    Email and password are required. Other fields are optional.
+    """
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(max_length=254, unique=True)
+    first_name = models.CharField(max_length=45, blank=True)
+    last_name = models.CharField(max_length=45, blank=True)
+    is_staff = models.BooleanField('staff status', default=False,
+                                   help_text='Designates whether the user can log into this admin '
+                                   'site.')
+    is_active = models.BooleanField('active', default=True,
+                                    help_text='Designates whether this user should be treated as '
+                                    'active. Unselect this instead of deleting accounts.')
+    date_joined = models.DateTimeField('joined on', default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        db_table = 'user'
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.email)
+
+    def get_full_name(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        return self.first_name
+
+    def email_user(self, subject, message, from_email=None):
+        send_mail(subject, message, from_email, [self.email])
+
+    def __unicode__(self):
+        full_name = '%s %s' % (self.first_name, self.last_name)
+        return full_name.strip()
+
+
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    permalink_title = models.CharField(max_length=45, unique=True)
+    description = models.TextField(blank=True)
+    role = models.CharField(max_length=45, blank=True)
+    client = models.CharField(max_length=80,blank=True)
+    date_started = models.DateTimeField('started on', blank=True, null=True)
+    date_completed = models.DateTimeField('completed on', blank=True, null=True)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'project'
+        verbose_name = 'project'
+        verbose_name_plural = 'projects'
+
+    def __unicode__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=45)
+    created = models.DateTimeField(default=timezone.now)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'tag'
+        verbose_name = 'tag'
+        verbose_name_plural = 'tags'
+
+    def __unicode__(self):
+        return self.name
