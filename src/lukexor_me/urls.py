e81ab4f8a82aa4c869844bd353b9b447352c96c4
Luke Petherbridge <lukexor@gmail.com>
2014-11-04 00:10:20 -0800

    Updated some strings and changed ids to classes

--- src/lukexor_me/urls.py
+++ src/lukexor_me/urls.py
@@ -1,23 +1,21 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import *
+from lukexor_me.views import HomeView, ArticlesView, ProjectsView, AboutView, ContactView, TitleView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^$', HomeView.as_view(), name='home'),
-    # url(r'articles/$', ArticlesView.as_view(), name='articles'),
-    url(r'articles/$', HomeView.as_view(), name='articles'),
-    # url(r'(?P<article_title>\w+)/$', HomeView.as_view()),
-    # url(r'^projects/$', ProjectsView.as_view(), name='projects'),
-    url(r'projects/$', HomeView.as_view(), name='projects'),
-    # url(r'^(?P<project_title>\w+)/$', HomeView.as_view()),
-    # url(r'^about/$', AboutView.as_view(), name='about'),
-    url(r'about/$', HomeView.as_view(), name='about'),
-    # url(r'^contact/$', ContactView.as_view(), name='contact'),
-    url(r'contact/$', HomeView.as_view(), name='contact'),
+    url(r'^articles/$', ArticlesView.as_view(), name='articles'),
+
+    url(r'^projects/$', ProjectsView.as_view(), name='projects'),
+    url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^subscribe/$', HomeView.as_view(), name='subscribe'),
+    url(r'^articles/(?P<year>\d{4})/$', HomeView.as_view()),
+    url(r'^(?P<title>\w+)/$', TitleView.as_view(), name='permalink'),
+
 
     url(r'^admin/', include(admin.site.urls), name='admin'),
 )
