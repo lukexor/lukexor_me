763168dc8a7ca295de4f858ad4bff112a2c6d8b2
Luke Petherbridge <lukexor@gmail.com>
2014-11-04 00:09:53 -0800

    Started contact page

--- src/lukexor_me/forms.py
+++ src/lukexor_me/forms.py
@@ -1,8 +1,11 @@
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from lukexor_me.models import CustomUser
 
+
 class CustomUserCreationForm(UserCreationForm):
+
     """
     A form that creates a user, with no privileges, from the given email and
     password.
@@ -16,7 +19,9 @@ class CustomUserCreationForm(UserCreationForm):
         model = CustomUser
         fields = ("email",)
 
+
 class CustomUserChangeForm(UserChangeForm):
+
     """A form for updating users. Includes all the fields on
     the user, but replaces the password field with admin's
     password hash display field.
@@ -29,3 +34,45 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
         fields = ("email",)
+
+
+class ContactForm(forms.Form):
+
+    """A simple message form
+    """
+
+    name = forms.CharField(
+        max_length=45,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'contact_name form-control',
+                'placeholder': 'Name',
+            }
+        ),
+    )
+    email = forms.EmailField(
+        max_length=254,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'contact_email form-control',
+                'placeholder': 'Email',
+            }
+        ),
+    )
+    phone = forms.CharField(
+        max_length=15,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'contact_phone form-control',
+                'placeholder': 'Phone',
+            }
+        ),
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'contact_message form-control',
+                'placeholder': 'Message',
+            }
+        ),
+    )
--- src/lukexor_me/views.py
+++ src/lukexor_me/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect, Http404
 from django.views.generic import View
+from django.core.mail import send_mail
+from lukexor_me.forms import ContactForm
 
 
 class HomeView(View):
@@ -21,6 +23,16 @@ class ProjectsView(View):
         return render(request, "projects.html")
 
 
+class TitleView(View):
+
+    def get(self, request, title=None):
+        title=None
+        if title:
+            return render(request, "%s.html" % (title))
+        else:
+            raise Http404
+
+
 class AboutView(View):
 
     def get(self, request):
@@ -30,4 +42,21 @@ class AboutView(View):
 class ContactView(View):
 
     def get(self, request):
-        return render(request, "contact.html")
+        if request.method == 'POST':
+            form = ContactForm(request.POST)
+
+            if form.is_valid():
+                name = form.cleaned_data['name']
+                sender = form.cleaned_data['email']
+                message = form.cleaned_data['message']
+                recipients = ['lukexor@gmail.com']
+
+                subject = "New message from %s on lukexor.me" % (name)
+
+                send_mail(subject, message, sender, recipients)
+
+                return HttpResponseRedirect('/thanks/')
+        else:
+            form = ContactForm()
+
+        return render(request, "contact.html", {'form': form})
