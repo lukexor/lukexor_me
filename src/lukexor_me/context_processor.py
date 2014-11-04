3482c6da013cf3dbe62fb739a97f6575afefb283
Luke Petherbridge <lukexor@gmail.com>
2014-11-04 00:08:52 -0800

    Added a strings constant

--- src/lukexor_me/context_processor.py
+++ src/lukexor_me/context_processor.py
@@ -5,9 +5,9 @@ def global_vars(request):
     """Returns a dict of variables
     """
     globals = {
-        'SITE_SUBTITLE': settings.SITE_SUBTITLE,
         'GA_CODE': settings.GA_CODE,
         'URLS': settings.URLS,
+        'STRINGS': settings.STRINGS,
     }
 
     return globals
