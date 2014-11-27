89259d59f807f2c056b7bcd9a19b24ff5d3d344e
Luke Petherbridge <lukexor@gmail.com>
2014-11-27 03:59:19 +0000

    Updated fab deploy

--- fabfile.py
+++ fabfile.py
@@ -1,15 +1,10 @@
 from fabric.api import local
 from fabric.api import lcd
 
-# def prepare_deployment(branch_name):
-#     local('python manage.py test main')
-#     local('python manage.py test blog')
-#     local('git add -p && git commit')
-#     local('git checkout master && git merge ' + branch_name)
-
 def deploy():
-    with lcd('/path/to/my/prod/area/'):
-        # local('git pull /my/path/to/dev/area/')
-        local('python manage.py migrate')
-        local('python manage.py test')
-        local('/my/command/to/restart/webserver')
+    local('git pull')
+    local('pip install --upgrade -r src/requirements.txt')
+    local('python src/manage.py migrate')
+    local('python src/manage.py test')
+    local('python src/manage.py collectstatic --noinput --clear --link')
+    local('sudo service apache2 graceful')
