a53da14f132882dcc5cea064d1653efaf118df32
Luke Petherbridge <lukexor@gmail.com>
2014-12-17 13:55:30 -0800

    Applied google javascript coding guidelines

--- src/static/scripts/main.js
+++ src/static/scripts/main.js
@@ -1,53 +1,50 @@
-$(document).ready(function ()
-{
-    // Set active tab
-    var current_page = window.location.pathname;
-    $('.nav-tabs a[href="' + current_page + '"]').parent('li').addClass('active');
+$(document).ready(function () {
+  // Set active tab
+  var currentPage = window.location.pathname;
 
-    if ( window.location.search == '' )
-    {
-        $('a[href="' + current_page + '"]').addClass('disabled');
-    }
+  // Sets the navigation tab as active by hilighting it
+  $('.nav-tabs a[href="' + currentPage + '"]').parent('li').addClass('active');
 
+  // Disables any links on the page that match the current URL
+  if ( window.location.search == '' ) {
+    $('a[href="' + currentPage + '"]').addClass('disabled');
+  }
 
-    // form validation
-    $('#send-message').addClass('disabled'); // Set default to disabled
-    $('#post-comment').addClass('disabled'); // Set default to disabled
+  // Prevent disabled links from functioning
+  $('body').on('click', 'a.disabled', function(event) {
+    event.preventDefault();
+  });
 
-    $('input').on('keyup blur change', function() { validate_form() });
-    $('textarea').on('keyup blur change', function() { validate_form() });
+  // Form validation
+  // Submit buttons are disabled by default until all required forms have
+  // values
+  $('#send-message').addClass('disabled'); // Set default to disabled
+  $('#post-comment').addClass('disabled'); // Set default to disabled
 
-    // Prevent disabled links from functioning
-    $('body').on('click', 'a.disabled', function(event) {
-        event.preventDefault();
-    });
+  $('input').on('keyup blur change', function() { validateForm() });
+  $('textarea').on('keyup blur change', function() { validateForm() });
 }); // end document.ready
 
-function validate_form()
-{
-    var inputs = $('input.required'),
-        textareas = $('textarea.required'),
-        button = $('button[type="submit"]');
+function validateForm() {
+  var inputs = $('input.required'),
+      textareas = $('textarea.required'),
+      button = $('button[type="submit"]');
 
-    fields = $.merge(inputs, textareas);
+  fields = $.merge(inputs, textareas);
 
-    var required_count = fields.size(),
-        valid_count = 0;
+  var requiredCount = fields.size(),
+      validCount = 0;
 
-    $.each(fields, function(c, field) {
-        if ( $(field).val() ) { valid_count += 1; }
-    });
+  // Ensure all required fields have values
+  $.each(fields, function(index, field) {
+    if ( $(field).val() ) { validCount += 1; }
+  });
 
-    console.log("Valid: " + valid_count + ". Required: " + required_count);
-
-    if ( valid_count === required_count )
-    {
-        button.removeClass('disabled');
-        return true;
-    }
-    else
-    {
-        button.addClass('disabled');
-        return false;
-    }
+  if ( validCount === requiredCount ) {
+    button.removeClass('disabled');
+    return true;
+  } else {
+    button.addClass('disabled');
+    return false;
+  }
 }
