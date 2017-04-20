614fd44579986c3c877b23b79b0dc534bd4e417c
Luke Petherbridge <lukexor@gmail.com>
2017-04-20 23:48:32 +0000

    Moved restart after failure check

--- bin/renew_cert.sh
+++ bin/renew_cert.sh
@@ -4,11 +4,11 @@
 certbot renew > /root/cert-renew.log 2>&1
 LE_STATUS=$?
 
-# Restart services
-/usr/bin/systemctl restart nginx
-
 if [ "$LT_STATUS" != 0 ]; then
 	echo Automated SSL renewal failed:
 	cat /root/cert-renew.log
 	exit 1
 fi
+
+# Restart services
+/usr/bin/systemctl restart nginx
